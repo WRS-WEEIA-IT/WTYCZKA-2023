cdf8300dd28a2cbfddcb0a818d37f6bf9df05473
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-09-19 22:00:22 +0200

    setup paymentsForm onSubmit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRpYRUv0AjaNPScq6rkoSq8l1gxxhWcvU",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
